82140ea99920fd0aebf0c18fa1f47ccdd38cc475
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2022-01-13 02:25:02 +0200

    anykernel: Rebrand for our use case
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Eva Kernel by m_vaisakh @ xda-developers
+kernel.string=Sleepy Kernel by LinkBoi00
 do.devicecheck=1
 do.modules=1
 do.systemless=1
